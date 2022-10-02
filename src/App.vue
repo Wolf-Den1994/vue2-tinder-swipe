ec048f8487261a7274d83ed0ddcbc2ab0c0a3efe
Denis <wolf-den@tut.by>
2022-10-02 17:51:11 +0300

    feat: add hummer

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TinderSwipe />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TinderSwipe from './TinderSwipe.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TinderSwipe
   }
 }
 </script>
 
 <style lang="scss">
+body {
+  max-width: 360px;
+  width: 100%;
+  overflow: hidden;
+}
+
 #app {
+  width: 100%;
+  height: 80vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/TinderSwipe.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="tinder--cards">
+    <div
+      v-for="(item, index) in arrayData"
+      :key="item"
+      class="tinder--card"
+      :style="{
+        'z-index': arrayData.length - index,
+        transform:
+          'scale(' + (20 - index) / 20 + ') translateY(' + 30 * index + 'px)',
+        opacity: (10 - index) / 10,
+      }"
+      v-hammer:pan="onPan"
+      v-hammer:panend="onPanEnd"
+    >
+      Card {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TinderSwipe",
+  data() {
+    return {
+      arrayData: [1, 2, 3, 4, 5, 6],
+    };
+  },
+  methods: {
+    initCards() {
+      const newCards = document.querySelectorAll(".tinder--card:not(.removed)");
+
+      newCards.forEach((card, index) => {
+        card.style.zIndex = this.arrayData.length - index;
+        card.style.transform =
+          "scale(" + (20 - index) / 20 + ") translateY(" + 30 * index + "px)";
+        card.style.opacity = (10 - index) / 10;
+      });
+    },
+    onPan(event) {
+      const target = event.target;
+      const el = target.closest(".tinder--card");
+      el.classList.add("moving");
+
+      if (event.deltaX === 0) return;
+      if (event.center.x === 0 && event.center.y === 0) return;
+
+      const xMulti = event.deltaX * 0.03;
+      const yMulti = event.deltaY / 80;
+      const rotate = xMulti * yMulti;
+
+      el.style.transform =
+        "translate(" +
+        event.deltaX +
+        "px, " +
+        event.deltaY +
+        "px) rotate(" +
+        rotate +
+        "deg)";
+    },
+    onPanEnd(event) {
+      const target = event.target;
+      const el = target.closest(".tinder--card");
+
+      el.classList.remove("moving");
+
+      const moveOutWidth = document.body.clientWidth;
+      const keep = Math.abs(event.deltaX) < 80 || Math.abs(event.velocityX) < 0.5;
+
+      el.classList.toggle("removed", !keep);
+
+      if (keep) {
+        el.style.transform = "";
+      } else {
+        const endX = Math.max(
+          Math.abs(event.velocityX) * moveOutWidth,
+          moveOutWidth
+        );
+        const toX = event.deltaX > 0 ? endX : -endX;
+        const endY = Math.abs(event.velocityY) * moveOutWidth;
+        const toY = event.deltaY > 0 ? endY : -endY;
+        const xMulti = event.deltaX * 0.03;
+        const yMulti = event.deltaY / 80;
+        const rotate = xMulti * yMulti;
+
+        el.style.transform =
+          "translate(" +
+          toX +
+          "px, " +
+          (toY + event.deltaY) +
+          "px) rotate(" +
+          rotate +
+          "deg)";
+        this.initCards();
+      }
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.tinder--cards {
+  height: 100%;
+  flex-grow: 1;
+  padding-top: 40px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+}
+
+.tinder--card {
+  display: inline-block;
+  width: 200px;
+  height: 300px;
+  border: 1px solid #aaa;
+  padding: 5px 5px 40px;
+  background: #eee;
+  border-radius: 8px;
+  overflow: hidden;
+  position: absolute;
+  will-change: transform;
+  transition: all 0.3s ease-in-out;
+  cursor: grab;
+
+  &.moving {
+    transition: none;
+    cursor: grabbing;
+  }
+}
+</style>
